74c0aa7cc7fc033bbc96211e3696b3661ed00c9a
fengxiaowei <viperdlll@126.com>
2015-05-31 00:47:54 +0800

    5

--- www/template/default/view/article/cxy/cxy.html.php
+++ www/template/default/view/article/cxy/cxy.html.php
@@ -15,15 +15,15 @@
 <div class="center">
     <div class="center_txt">
         	<div class="center_list_1 p1">
-            	<a href="?m=article&f=browse&categoryID=83&pt=cxy&type=list"><img src="<?php echo $themeRoot?>cxy/images/1_title.png"></a>
+            	<a href="?m=article&f=browse&categoryID=83&pt=cxy&type=list"><img src="<?php echo $themeRoot?>cxy/images/1_title.png" width="81" height="227"></a>
 
         </div>
       <div class="center_list_2 p2">
-      	<a href="?m=article&f=browse&categoryID=84&pt=cxy&type=2"><img src="<?php echo $themeRoot?>cxy/images/2_title.png"></a>
+      	<a href="?m=article&f=browse&categoryID=84&pt=cxy&type=2"><img src="<?php echo $themeRoot?>cxy/images/2_title.png" width="263" height="67"></a>
 
       </div>
       <div class="center_list_1 p3">
-      	<a href="?m=article&f=browse&categoryID=85&pt=cxy&type=list"><img src="<?php echo $themeRoot?>cxy/images/3_title.png"></a>
+      	<a href="?m=article&f=browse&categoryID=85&pt=cxy&type=list"><img src="<?php echo $themeRoot?>cxy/images/3_title.png" width="264" height="65"></a>
 
       </div>
       <div class="center_list_2 p4">
